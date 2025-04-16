e1b67dfee9150b80d15299072023fce5fe814ae1
Danny <g00437615@atu.ie>
2025-04-16 18:26:55 +0100

    added javadoc and bigO to FileManager.java

--- DSAProject/src/ie/atu/sw/FileManager.java
+++ DSAProject/src/ie/atu/sw/FileManager.java
@@ -7,9 +7,24 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * file manager handles reading and writing text files for the encoder and decoder
+ * i use this to get input files and save output so nothing is hardcoded
+ * 
+ * reading is just line by line so its O(n) and writing same idea
+ */
 public class FileManager {
 	
-	// Reading text from a file line by line
+	/**
+	 * reads a txt file from disk and stores each line into a list
+	 *
+	 * @param path the file path that the user gave from the menu
+	 * @return a list of strings, each string is one line from the file
+	 * @throws Exception if something goes wrong with the file
+	 *
+	 * Big-O: O(n) where n is number of lines in the file
+	 * each line is added to list which is fast
+	 */
 	public List<String> readTextFile(String path) throws Exception {
 		List<String> lines = new ArrayList<>();
 		BufferedReader reader = new BufferedReader(new FileReader(path));
@@ -22,7 +37,18 @@ public class FileManager {
 		reader.close();
 		return lines;
 	}
-	
+
+	/**
+	 * writes a list of lines to a text file on disk
+	 * each string in the list becomes its own line in the file
+	 * 
+	 * @param path the file path to write to
+	 * @param lines the data to write
+	 * @throws Exception if writing fails
+	 *
+	 * Big-O: O(n) where n is number of lines to write
+	 * loop just goes through the list once
+	 */
 	public void writeTextFile(String path, List<String> lines) throws Exception {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(path));
 		
@@ -33,5 +59,4 @@ public class FileManager {
 		
 		writer.close();
 	}
-
-}
+} 
